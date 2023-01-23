4033749af13678020576b2905c5f46d7cdb8a2ac
skebaas <54587831+skebaas@users.noreply.github.com>
2023-01-23 11:54:22 -0500

    Updated script to fit PEP-8 Standard

--- QC/Nifti_QC/parse_json.py
+++ QC/Nifti_QC/parse_json.py
@@ -6,67 +6,76 @@ import glob
 import sys, getopt
 import pandas as pd 
 
+
 def main(argv):
-	inputfile = ''
-	outputfile = 'Subjects_default'
+    # Define input and output file variables with default values
+    inputfile = ''
+    outputfile = 'Subjects_default'
 
-	try:
-		opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-	except getopt.GetoptError:
-		print("parse_json.py -i <subject directory> -o <outputfile>")
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt == "-h":
-			print("parse_json.py -i <subject directory> -o <outputfile>")
-			sys.exit()
-		elif opt in ("-i", "--ifile"):
-			inputfile = os.path.abspath(arg)
-		elif opt in ("-o", "--ofile"):
-			outputfile = arg
+    # Try to get input and output file options from command line arguments
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print("parse_json.py -i <subject directory> -o <outputfile>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("parse_json.py -i <subject directory> -o <outputfile>")
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = os.path.abspath(arg)
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
 
-	df = pd.DataFrame(columns=["File_Name", "Coil_Name", "Phase_Direction", "Echo_Time"])
-	subjects_path = os.path.join(inputfile, 'sub-*')
-	subjects = glob.glob(subjects_path)
-	for subject in subjects:
-		files_path = os.path.join(subject, 'ses-1/func/*json')
-		files = glob.glob(files_path)
-		for file in files:
-			filename = os.path.basename(file)
-			json = search_json(file)
-			df = add_row(filename, json, df)
-	print(f"Outputting data to {outputfile}.csv")
-	df.to_csv(f'{outputfile}.csv')
-def search_json(inputfile):
-	'''
-	Returns a json as a dictionary
+    # Create a DataFrame with specified columns
+    df = pd.DataFrame(columns=["File_Name", "Coil_Name", "Phase_Direction", "Echo_Time"])
 
-	Parameters:
-		inputfile (str): Path to json file
+    # Get list of all subjects in the input directory
+    subjects_path = os.path.join(inputfile, 'sub-*')
+    subjects = glob.glob(subjects_path)
 
-	Returns:
-		json_file (obj): 
-	'''
-	try:
-		with open(inputfile, 'r') as f:
-			json_file = json.load(f)
-			return json_file
-	except KeyError:
-		return -1
+    # Iterate through each subject
+    for subject in subjects:
+        # Get list of all json files in the subject's session 1 functional directory
+        files_path = os.path.join(subject, 'ses-1/func/*json')
+        files = glob.glob(files_path)
 
-def add_row(filename, json_file, df):
-	'''
-	Returns a new dataframe after adding a row to the input dataframe 'df'
+        # Iterate through each json file
+        for file in files:
+            filename = os.path.basename(file)
+            json = search_json(file)
+            df = add_row(filename, json, df)
 
-	Parameters:
-		filename (str): Path to json file
-		json_file (str): Dictionary containing json information
-		df (DataFrame): Pandas dataframe which the row will be appended to 
-	'''
-	names = filename.split("_")
-	name = f"{names[0]}_{names[2]}_{names[-2]}"
-	row = pd.Series(data={'File_Name': name, 'Coil_Name': json_file["ReceiveCoilName"], 'Phase_Direction': json_file["PhaseEncodingDirection"], 'Echo_Time': json_file["EchoTime"]})
-	df = df.append(row, ignore_index=True)
-	return df
+    # Output the DataFrame to a csv file
+    print(f"Outputting data to {outputfile}.csv")
+    df.to_csv(f'{outputfile}.csv')
+
+def search_json(inputfile: str) -> dict:
+    """
+    Returns a json as a dictionary
+    :param inputfile: Path to json file
+    :return: json_file or -1 if there is a KeyError
+    """
+    try:
+        with open(inputfile, 'r') as f:
+            json_file = json.load(f)
+            return json_file
+    except KeyError:
+        return -1
+
+def add_row(filename: str, json_file: dict, df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a new dataframe after adding a row to the input dataframe 'df'
+    :param filename: Path to json file
+    :param json_file: Dictionary containing json information
+    :param df: Pandas dataframe which the row will be appended to
+    :return: df with a new row appended
+    """
+    names = filename.split("_")
+    name = f"{names[0]}_{names[2]}_{names[-2]}"
+    row = pd.Series(data={'File_Name': name, 'Coil_Name': json_file["ReceiveCoilName"], 'Phase_Direction': json_file["PhaseEncodingDirection"], 'Echo_Time': json_file["EchoTime"]})
+    df = df.append(row, ignore_index=True)
+    return df
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
