daaad62c0bed66bd70f91b71d596e7260ac060f2
skebaas <54587831+skebaas@users.noreply.github.com>
2023-01-23 15:36:54 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'nipype',
+        'nibable',
+        'numpy'
+    ],
+    # Ubuntu specific installations
+    extras_require={
+        'Ubuntu': ['fsl','freesurfer','matlab','ANTs']
+    }
+)
