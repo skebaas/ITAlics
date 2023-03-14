a3f8a4c7dd48d71662d73180f606547f3eb03d31
skebaas <skebaas@upmc.edu>
2023-03-14 12:53:29 -0400

    updated accuracy file arguments

--- code/QC/scripts/read_accuracy_mat_files.py
+++ code/QC/scripts/read_accuracy_mat_files.py
@@ -5,20 +5,23 @@ import scipy.io
 import pandas as pd
 import os
 
-def read_accuracy_data(file_pattern):
-    data = {'ID': [], 'Accuracy': []}  # dictionary to store data
-    
+def read_accuracy_data(files):
+    data = {'ID': [], 'Run': [],'Accuracy': [], 'Average': []}  # dictionary to store data
+    print(files)
     # loop over matching .mat files
-    for i, file_name in enumerate(glob.glob(file_pattern)):
+    for i, file_name in enumerate(files):
         # load .mat file
         mat_file = scipy.io.loadmat(file_name)
-        
+        id_name = os.path.basename(file_name).split('.')[1]
+        run_name = os.path.basename(file_name).split('_')[1]
         # extract accuracy information
         accuracy = mat_file['accuracy'][0][0][0]
-        
+        print(type(accuracy))
         # add data to dictionary
-        data['ID'].append(i+1)
+        data['ID'].append(id_name)
+        data['Run'].append(run_name)
         data['Accuracy'].append(accuracy)
+        data['Average'].append(accuracy.mean())
     
     # create pandas dataframe
     df = pd.DataFrame(data)
@@ -27,13 +30,18 @@ def read_accuracy_data(file_pattern):
 
 # example usage
 if __name__ == '__main__':
-    if len(sys.argv) < 2 or sys.argv[1] in ['-h', '--help']:
-        print(f'Usage: {sys.argv[0]} <file_pattern>')
-        print(f'Example {sys.argv[0]} acc_efnback1_*.mat')
+    if len(sys.argv) < 3 or sys.argv[1] in ['-h', '--help']:
+        print(f'Usage: {sys.argv[0]} <file_pattern> <output_name>')
+        print(f'Example {sys.argv[0]} acc_efnback1_*.mat accuracies_efnback.txt')
         sys.exit()
     
-    file_pattern = sys.argv[1]
-    file_patter = os.path.abspath(file_pattern)
-    df = read_accuracy_data(file_pattern)
+    file_pattern = str(sys.argv[1])
+    output_name = str(sys.argv[2])
+    #file_pattern = os.path.abspath(file_pattern)
+    files = glob.glob(file_pattern)
+    df = read_accuracy_data(files)
+    df.set_index('ID', inplace=True)
+    df.sort_index(inplace=True)
+    df.to_csv(output_name)
     print(df)
 
